601718875d5c7f336c3398a04efb06a87128be7f
Sifat2245 <Sifat2245@users.noreply.github.com>
2025-08-22 09:51:46 +0600

    Project Initialization

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+        <ul>
+            <li>home</li>
+            <li>about</li>
+            <li>services</li>
+        </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
